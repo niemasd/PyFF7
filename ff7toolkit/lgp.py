c86cc3b0908a2f8db9bd5977b3f91cd5b93b768f
Niema Moshiri <niemamoshiri@gmail.com>
2019-04-30 15:00:23 -0700

    Restructured and added ability to load data from LGP

--- ff7toolkit/lgp.py
+++ ff7toolkit/lgp.py
@@ -9,20 +9,25 @@ from struct import unpack
 # size of various items in an LGP archive (in bytes)
 SIZE = {
     # Header
-    'HEADER_FILE-CREATOR': 12, # File Creator
-    'HEADER_NUM-FILES':     4, # Number of Files in Archive
+    'HEADER_FILE-CREATOR':  12, # File Creator
+    'HEADER_NUM-FILES':      4, # Number of Files in Archive
 
     # Table of Contents Entries
-    'TOC-ENTRY_FILENAME':  20, # ToC Entry: Filename
+    'TOC-ENTRY_FILENAME':   20, # ToC Entry: Filename
     'TOC-ENTRY_DATA-START':  4, # ToC Entry: Data Start Position
     'TOC-ENTRY_CHECK':       1, # ToC Entry: Check Code
     'TOC-ENTRY_DUP-IDENT':   2, # ToC Entry: Duplicate Filename Identifier
 
     # CRC
     'CRC-DEFAULT':        3602, # CRCs are usually 3602, but not necessarily
+
+    # Data Entries
+    'DATA-ENTRY_FILENAME':  20, # Data Entry: Filename
+    'DATA-ENTRY_FILESIZE':   4, # Data Entry: File Size
 }
 SIZE['HEADER'] = sum(SIZE[k] for k in SIZE if k.startswith('HEADER_'))
 SIZE['TOC-ENTRY'] = sum(SIZE[k] for k in SIZE if k.startswith('TOC-ENTRY_'))
+SIZE['DATA-ENTRY_HEADER'] = sum(SIZE[k] for k in SIZE if k.startswith('DATA-ENTRY_'))
 
 # start positions of various items in an LGP archive (in bytes)
 START = {
@@ -38,16 +43,22 @@ START['TOC-ENTRY_FILENAME'] = 0
 START['TOC-ENTRY_DATA-START'] = START['TOC-ENTRY_FILENAME'] + SIZE['TOC-ENTRY_FILENAME']
 START['TOC-ENTRY_CHECK'] = START['TOC-ENTRY_DATA-START'] + SIZE['TOC-ENTRY_DATA-START']
 START['TOC-ENTRY_DUP-IDENT'] = START['TOC-ENTRY_CHECK'] + SIZE['TOC-ENTRY_CHECK']
+# Data entries (0 = start of entry)
+START['DATA-ENTRY_FILENAME'] = 0
+START['DATA-ENTRY_FILESIZE'] = START['DATA-ENTRY_FILENAME'] + SIZE['DATA-ENTRY_FILENAME']
 
 class LGP:
     '''LGP Archive class'''
-    def __init__(self, filename):
+    def __init__(self, filename, load_data=True):
         '''``LGP`` constructor
 
         Args:
             ``filename`` (``str``): The filename of the LGP archive
+
+            ``load_data`` (``bool``): Load the actual data (not recommended for large files)
         '''
         self.filename = filename
+        self.data = None
         with open(filename, 'rb') as f:
             # read header
             tmp = f.read(SIZE['HEADER'])
@@ -60,7 +71,6 @@ class LGP:
             self.toc = list()
             for i in range(self.header['num_files']):
                 tmp = f.read(SIZE['TOC-ENTRY'])
-                start = START['TOC'] + (i * SIZE['TOC-ENTRY'])
                 self.toc.append({
                     'filename': tmp[START['TOC-ENTRY_FILENAME']:START['TOC-ENTRY_FILENAME']+SIZE['TOC-ENTRY_FILENAME']].decode(),
                     'data_start': unpack('i', tmp[START['TOC-ENTRY_DATA-START']:START['TOC-ENTRY_DATA-START']+SIZE['TOC-ENTRY_DATA-START']])[0],
@@ -68,20 +78,13 @@ class LGP:
                     'dup-ident': unpack('h', tmp[START['TOC-ENTRY_DUP-IDENT']:START['TOC-ENTRY_DUP-IDENT']+SIZE['TOC-ENTRY_DUP-IDENT']])[0],
                 })
             self.crc = f.read(self.toc[0]['data_start'] - SIZE['HEADER'] - len(self.toc)*SIZE['TOC-ENTRY'])
-        self.data = None
-
-def read_lgp_info(filename):
-    '''Read the information about an LGP archive
-
-    Args:
-        ``filename`` (``str``): Name of the LGP archive from which to read info
-
-    Returns:
-        ``dict``: Archive Header (keys are ``"file_creator"`` and ``"num_files"``)
-
-        ``dict``: Archive Table of Contents (list of ToC entries)
-
-        ``bytes``: CRC byte representation
-    '''
-    lgp_archive = LGP(filename)
-    return lgp_archive.header, lgp_archive.toc, lgp_archive.crc
+            if load_data:
+                self.data = list()
+                for i in range(self.header['num_files']):
+                    tmp = f.read(SIZE['DATA-ENTRY_HEADER'])
+                    tmp = {
+                        'filename': tmp[START['DATA-ENTRY_FILENAME']:START['DATA-ENTRY_FILENAME']+SIZE['DATA-ENTRY_FILENAME']].decode(),
+                        'filesize': unpack('i', tmp[START['DATA-ENTRY_FILESIZE']:START['DATA-ENTRY_FILESIZE']+SIZE['DATA-ENTRY_FILESIZE']])[0],
+                    }
+                    tmp['data'] = f.read(tmp['filesize'])
+                    self.data.append(tmp)
--- lgp_info.py
+++ lgp_info.py
@@ -4,22 +4,22 @@ Read the information of an LGP archive
 Niema Moshiri 2019
 '''
 from ff7toolkit import NULL_STR
-from ff7toolkit.lgp import read_lgp_info
+from ff7toolkit.lgp import LGP
 from sys import argv
 USAGE = "USAGE: %s <lgp_file>" % argv[0]
 
 if __name__ == "__main__":
     if len(argv) != 2:
         print(USAGE); exit(1)
-    header,toc,crc = read_lgp_info(argv[1])
-    if header['num_files'] != len(toc):
+    lgp = LGP(argv[1], load_data=False)
+    if lgp.header['num_files'] != len(lgp.toc):
         raise ValueError("Number of files in header doesn't match Table of Contents length")
     print("Information")
-    print("* File Creator: %s" % header['file_creator'].strip(NULL_STR))
-    print("* Number of Files: %d" % header['num_files'])
-    print("* Length of CRC: %d bytes" % len(crc))
+    print("* File Creator: %s" % lgp.header['file_creator'].strip(NULL_STR))
+    print("* Number of Files: %d" % lgp.header['num_files'])
+    print("* Length of CRC: %d bytes" % len(lgp.crc))
     print()
     print("Table of Contents")
     print("* FILENAME\tSTART\tCHECK\tDUPLICATE IDENTIFIER")
-    for entry in toc:
+    for entry in lgp.toc:
         print("* %s\t%d\t%d\t%d" % (entry['filename'].strip(NULL_STR), entry['data_start'], entry['check'], entry['dup-ident']))
