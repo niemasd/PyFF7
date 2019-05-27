d2a47bfe941a7394634a91d1722c9d75a37637be
Niema Moshiri <niemamoshiri@gmail.com>
2019-05-27 00:32:21 -0700

    Simplified script

--- tex_convert.py
+++ tex_convert.py
@@ -10,5 +10,4 @@ USAGE = "USAGE: %s <input_tex_file> <output_image_file>" % argv[0]
 if __name__ == "__main__":
     if len(argv) != 3:
         print(USAGE); exit(1)
-    img = TEX(argv[1]).get_pillow_image()
-    img.save(argv[2])
+    TEX(argv[1]).get_pillow_image().save(argv[2])
