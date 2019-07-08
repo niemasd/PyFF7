b4bcf51ee23c0ad6e194ec94c97a6e834016ed82
Niema Moshiri <niemamoshiri@gmail.com>
2019-07-08 15:33:11 -0700

    More updates to save

--- PyFF7/save.py
+++ PyFF7/save.py
@@ -11,6 +11,7 @@ SAVE_SLOT_SIZE = 4340
 
 # start offsets of various items in a save file (in bytes, with respect to start of slot data)
 START = {
+    # Final Fantasy VII Save Slot
     'SLOT_CHECKSUM':          0x0000, # Save Slot: Checksum
     'SLOT_UNKNOWN1':          0x0002, # Save Slot: Unknown 1
     'SLOT_PREVIEW-LEVEL':     0x0004, # Save Slot: Preview: Lead Character's Level
@@ -25,6 +26,34 @@ START = {
     'SLOT_PREVIEW-GIL':       0x0020, # Save Slot: Preview: Total Gil
     'SLOT_PREVIEW-PLAYTIME':  0x0024, # Save Slot: Preview: Total Playtime
     'SLOT_PREVIEW-LOCATION':  0x0028, # Save Slot: Preview: Save Location
+    'SLOT_WINDOW-COLOR-UL':   0x0048, # Save Slot: Window Color, Upper-Left Corner (RGB)
+    'SLOT_WINDOW-COLOR-UR':   0x004B, # Save Slot: Window Color, Upper-Right Corner (RGB)
+    'SLOT_WINDOW-COLOR-LL':   0x004E, # Save Slot: Window Color, Lower-Left Corner (RGB)
+    'SLOT_WINDOW-COLOR-LR':   0x0051, # Save Slot: Window Color, Lower-Right Corner (RGB)
+    'SLOT_RECORD-CLOUD':      0x0054, # Save Slot: Character Record: Cloud
+
+    # Character Record
+    'RECORD_SEPHIROTH-FLAG':    0x00, # Character Record: Vincent -> Sephiroth Flag
+    'RECORD_LEVEL':             0x01, # Character Record: Level (0-99)
+    'RECORD_STAT-STRENGTH':     0x02, # Character Record: Status: Strength (0-255)
+    'RECORD_STAT-VITALITY':     0x03, # Character Record: Status: Vitality (0-255)
+    'RECORD_STAT-MAGIC':        0x04, # Character Record: Status: Magic (0-255)
+    'RECORD_STAT-SPIRIT':       0x05, # Character Record: Status: Spirit (0-255)
+    'RECORD_STAT-DEXTERITY':    0x06, # Character Record: Status: Dexterity (0-255)
+    'RECORD_STAT-LUCK':         0x07, # Character Record: Status: Luck (0-255)
+    'RECORD_BONUS-STRENGTH':    0x08, # Character Record: Bonus: Strength
+    'RECORD_BONUS-VITALITY':    0x09, # Character Record: Bonus: Vitality
+    'RECORD_BONUS-MAGIC':       0x0A, # Character Record: Bonus: Magic
+    'RECORD_BONUS-SPIRIT':      0x0B, # Character Record: Bonus: Spirit
+    'RECORD_BONUS-DEXTERITY':   0x0C, # Character Record: Bonus: Dexterity
+    'RECORD_BONUS-LUCK':        0x0D, # Character Record: Bonus: Luck
+    'RECORD_LIMIT-LEVEL':       0x0E, # Character Record: Current Limit Level (1-4)
+    'RECORD_LIMIT-BAR':         0x0F, # Character Record: Current Limit Bar (0 = Empty, 255 = Limit Break)
+    'RECORD_NAME':              0x10, # Character Record: Name
+    'RECORD_WEAPON':            0x1C, # Character Record: Equipped Weapon
+    'RECORD_ARMOR':             0x1D, # Character Record: Equipped Armor
+    'RECORD_ACCESSORY':         0x1E, # Character Record: Equipped Accessory
+    'RECORD_FLAGS':             0x1F, # Character Record: Character Flags
 }
 
 # size of various items in a save file (in bytes)
@@ -40,10 +69,23 @@ SIZE = {
     'SLOT_PREVIEW-MP-MAX':    2, # Save Slot: Preview: Lead Character's Maximum MP
     'SLOT_PREVIEW-NAME':     16, # Save Slot: Preview: Lead Character's Name
     'SLOT_PREVIEW-PLAYTIME':  4, # Save Slot: Preview: Total Playtime
-    'SLOT_PREVIEW-PORTRAIT1': 1, # Save Slot: Preview: Portrait 1
-    'SLOT_PREVIEW-PORTRAIT2': 1, # Save Slot: Preview: Portrait 2
-    'SLOT_PREVIEW-PORTRAIT3': 1, # Save Slot: Preview: Portrait 3
+    'SLOT_PREVIEW-PORTRAIT':  1, # Save Slot: Preview: Portrait
+    'SLOT_RECORD':          132, # Save Slot: Character Record
     'SLOT_UNKNOWN1':          2, # Save Slot: Unknown 1
+    'SLOT_WINDOW-COLOR':      3, # Save Slot: Window Color (RGB)
+
+    # Character Record
+    'RECORD_ACCESSORY':       1, # Character Record: Accessory
+    'RECORD_ARMOR':           1, # Character Record: Armor
+    'RECORD_BONUS':           1, # Character Record: Bonus
+    'RECORD_FLAGS':           3, # Character Record: Character Flags
+    'RECORD_LEVEL':           1, # Character Record: Level (0-99)
+    'RECORD_LIMIT-BAR':       1, # Character Record: Current Limit Bar (0 = Empty, 255 = Limit Break)
+    'RECORD_LIMIT-LEVEL':     1, # Character Record: Current Limit Level (1-4)
+    'RECORD_NAME':           12, # Character Record: Name
+    'RECORD_SEPHIROTH-FLAG':  1, # Character Record: Vincent -> Sephiroth Flag
+    'RECORD_STAT':            1, # Character Record: Status
+    'RECORD_WEAPON':          1, # Character Record: Equipped Weapon
 }
 
 # translate portrait number to character name
@@ -141,6 +183,61 @@ FILESIZE_TO_FORMAT = {PROP[k]['file_size']:k for k in PROP}
 # error messages
 ERROR_INVALID_SAVE_FILE = "Invalid save file"
 
+def parse_color(data):
+    '''Parse the bytes of an RGB color
+
+    Args:
+        ``data`` (``bytes``): The input RGB color bytes
+
+    Returns:
+        ``tuple`` of ``int``: The resulting RGB color
+    '''
+    if len(data) != SIZE['SLOT_WINDOW-COLOR']:
+        raise ValueError("Invalid color data length: %d bytes" % len(data))
+    return (data[0], data[1], data[2])
+
+def parse_char_flags(data):
+    '''Parse the bytes of Character Flags (in Character Record)
+
+    Args:
+        ``data`` (``bytes``): The input character flags bytes
+
+    Returns:
+        TODO: The resulting character flags
+    '''
+    if len(data) != SIZE['RECORD_FLAGS']:
+        raise ValueError("Invalid character flags data length: %d" % len(data))
+    return data
+
+def parse_char_record(data):
+    '''Parse the bytes of a character record
+
+    Args:
+        ``data`` (``bytes``): The input character record bytes
+
+    Returns:
+        ``dict``: The resulting character record
+    '''
+    if len(data) != SIZE['SLOT_RECORD']:
+        raise ValueError("Invalid character record data length: %d bytes" % len(data))
+    out = dict()
+    out['sephiroth_flag'] = unpack('B', data[START['RECORD_SEPHIROTH-FLAG']:START['RECORD_SEPHIROTH-FLAG']+SIZE['RECORD_SEPHIROTH-FLAG']])[0]
+    out['level'] = unpack('B', data[START['RECORD_LEVEL']:START['RECORD_LEVEL']+SIZE['RECORD_LEVEL']])[0]
+    out['status'] = dict()
+    for k in ['STRENGTH', 'VITALITY', 'MAGIC', 'SPIRIT', 'DEXTERITY', 'LUCK']:
+        out['status'][k.lower()] = unpack('B', data[START['RECORD_STAT-%s'%k]:START['RECORD_STAT-%s'%k]+SIZE['RECORD_STAT']])[0]
+    out['bonus'] = dict()
+    for k in ['STRENGTH', 'VITALITY', 'MAGIC', 'SPIRIT', 'DEXTERITY', 'LUCK']:
+        out['bonus'][k.lower()] = unpack('B', data[START['RECORD_BONUS-%s'%k]:START['RECORD_BONUS-%s'%k]+SIZE['RECORD_BONUS']])[0]
+    out['limit_level'] = unpack('B', data[START['RECORD_LIMIT-LEVEL']:START['RECORD_LIMIT-LEVEL']+SIZE['RECORD_LIMIT-LEVEL']])[0]
+    out['limit_bar'] = unpack('B', data[START['RECORD_LIMIT-BAR']:START['RECORD_LIMIT-BAR']+SIZE['RECORD_LIMIT-BAR']])[0]
+    out['name'] = decode_field_text(data[START['RECORD_NAME']:START['RECORD_NAME']+SIZE['RECORD_NAME']])
+    out['weapon'] = unpack('B', data[START['RECORD_WEAPON']:START['RECORD_WEAPON']+SIZE['RECORD_WEAPON']])[0]
+    out['armor'] = unpack('B', data[START['RECORD_ARMOR']:START['RECORD_ARMOR']+SIZE['RECORD_ARMOR']])[0]
+    out['accessory'] = unpack('B', data[START['RECORD_ACCESSORY']:START['RECORD_ACCESSORY']+SIZE['RECORD_ACCESSORY']])[0]
+    out['flags'] = parse_char_flags(data[START['RECORD_FLAGS']:START['RECORD_FLAGS']+SIZE['RECORD_FLAGS']])
+    return out
+
 def parse_slot_data(data):
     '''Parse the bytes of a save slot
 
@@ -152,12 +249,13 @@ def parse_slot_data(data):
     '''
     if len(data) != SAVE_SLOT_SIZE:
         raise ValueError(ERROR_INVALID_SAVE_FILE)
-    out = {'preview':dict()}
+    out = dict()
     out['checksum'] = unpack('H', data[START['SLOT_CHECKSUM']:START['SLOT_CHECKSUM']+SIZE['SLOT_CHECKSUM']])[0]
     out['unknown1'] = unpack('H', data[START['SLOT_UNKNOWN1']:START['SLOT_UNKNOWN1']+SIZE['SLOT_UNKNOWN1']])[0]
+    out['preview'] = dict()
     out['preview']['level'] = unpack('B', data[START['SLOT_PREVIEW-LEVEL']:START['SLOT_PREVIEW-LEVEL']+SIZE['SLOT_PREVIEW-LEVEL']])[0]
     for i in [1,2,3]:
-        out['preview']['portrait%d'%i] = unpack('B', data[START['SLOT_PREVIEW-PORTRAIT%d'%i]:START['SLOT_PREVIEW-PORTRAIT%d'%i]+SIZE['SLOT_PREVIEW-PORTRAIT%d'%i]])[0]
+        out['preview']['portrait%d'%i] = unpack('B', data[START['SLOT_PREVIEW-PORTRAIT%d'%i]:START['SLOT_PREVIEW-PORTRAIT%d'%i]+SIZE['SLOT_PREVIEW-PORTRAIT']])[0]
     out['preview']['name'] = decode_field_text(data[START['SLOT_PREVIEW-NAME']:START['SLOT_PREVIEW-NAME']+SIZE['SLOT_PREVIEW-NAME']])
     out['preview']['curr_hp'] = unpack('H', data[START['SLOT_PREVIEW-HP-CURR']:START['SLOT_PREVIEW-HP-CURR']+SIZE['SLOT_PREVIEW-HP-CURR']])[0]
     out['preview']['max_hp'] = unpack('H', data[START['SLOT_PREVIEW-HP-MAX']:START['SLOT_PREVIEW-HP-MAX']+SIZE['SLOT_PREVIEW-HP-MAX']])[0]
@@ -166,6 +264,12 @@ def parse_slot_data(data):
     out['preview']['gil'] = unpack('I', data[START['SLOT_PREVIEW-GIL']:START['SLOT_PREVIEW-GIL']+SIZE['SLOT_PREVIEW-GIL']])[0]
     out['preview']['playtime'] = unpack('I', data[START['SLOT_PREVIEW-PLAYTIME']:START['SLOT_PREVIEW-PLAYTIME']+SIZE['SLOT_PREVIEW-PLAYTIME']])[0]
     out['preview']['location'] = decode_field_text(data[START['SLOT_PREVIEW-LOCATION']:START['SLOT_PREVIEW-LOCATION']+SIZE['SLOT_PREVIEW-LOCATION']])
+    out['window_color'] = dict()
+    for k1,k2 in [('upper_left','UL'), ('upper_right','UR'), ('lower_left','LL'), ('lower_right','LR')]:
+        out['window_color'][k1] = parse_color(data[START['SLOT_WINDOW-COLOR-%s'%k2]:START['SLOT_WINDOW-COLOR-%s'%k2]+SIZE['SLOT_WINDOW-COLOR']])
+    out['record'] = dict()
+    for k in ['CLOUD']: # TODO ADD OTHER CHARACTERS
+        out['record'][k.lower()] = parse_char_record(data[START['SLOT_RECORD-%s'%k]:START['SLOT_RECORD-%s'%k]+SIZE['SLOT_RECORD']])
     return out
 
 class Save:
--- save_info.py
+++ save_info.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
                 print("  * Save Slot %d: Empty" % (i+1)); continue
             else:
                 print("  * Save Slot %d:" % (i+1))
-            print("    * Checksum: 0x%x" % s['data']['checksum'])
+            print("    * Checksum: 0x%04X" % s['data']['checksum'])
             print("    * Unknown 1: %d" % s['data']['unknown1'])
             print("    * Preview:")
             print("      * Lead Character's Level: %d" % s['data']['preview']['level'])
@@ -31,5 +31,26 @@ if __name__ == "__main__":
             print("      * Total Gil: %d" % s['data']['preview']['gil'])
             print("      * Playtime (seconds): %d" % s['data']['preview']['playtime'])
             print("      * Save Location: %s" % s['data']['preview']['location'])
+            print("    * Window Color:")
+            for k in ['upper_left', 'upper_right', 'lower_left', 'lower_right']:
+                print("      * %s: %s" % (k.replace('_',' ').capitalize(), '#%02X%02X%02X'%s['data']['window_color'][k]))
+            for k in ['Cloud']:
+                rec = s['data']['record'][k.lower()]
+                print("    * Character Record: %s" % k)
+                print("      * Name: %s" % rec['name'])
+                print("      * Vincent -> Sephiroth Flag: %s" % bool(rec['sephiroth_flag']))
+                print("      * Level: %d" % rec['level'])
+                print("      * Status:")
+                for k in ['strength','vitality','magic','spirit','dexterity','luck']:
+                    print("        * %s: %d" % (k.capitalize(), rec['status'][k]))
+                print("      * Bonus:")
+                for k in ['strength','vitality','magic','spirit','dexterity','luck']:
+                    print("        * %s: %d" % (k.capitalize(), rec['bonus'][k]))
+                print("      * Limit Level: %d" % rec['limit_level'])
+                print("      * Limit Bar: %d%%" % int(100.*rec['limit_bar']/255.))
+                print("      * Weapon: %d" % rec['weapon'])
+                print("      * Armor: %d" % rec['armor'])
+                print("      * Accessory: %d" % rec['accessory'])
+                print("      * Character Flags: %s" % ' '.join(bin(e).lstrip('0b').zfill(8) for e in rec['flags']))
     except BrokenPipeError:
         stderr.close()
